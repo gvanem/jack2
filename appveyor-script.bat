bcba30a69e73b6c068295f3be9c99b185f9fdead
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-02 12:49:32 +0000

    Drop the '-MP' compile since AppVeyor's 'mingw32-make' seems to be old (does not like the long cmd-line).

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -4,7 +4,7 @@ if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0
 
 if %1. == build. (
   cd %APPVEYOR_BUILD_FOLDER%\src
-  c:\MinGW\bin\mingw32-make -f Makefile.Windows CC=cl all
+  c:\MinGW\bin\mingw32-make -f Makefile.Windows USE_MP_COMPILE=0 CC=cl all
 ) else (
   echo Usage: %~dp0appveyor-script.bat "build"
 )
