cf3482ca59ea3cf1b09da87177bf050f25089aa5
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-02 12:47:48 +0000

    Try 'cl' since the 'clang-cl' on PATH is 64-bit.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -4,7 +4,7 @@ if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0
 
 if %1. == build. (
   cd %APPVEYOR_BUILD_FOLDER%\src
-  c:\MinGW\bin\mingw32-make -f Makefile.Windows CC=clang-cl all
+  c:\MinGW\bin\mingw32-make -f Makefile.Windows CC=cl all
 ) else (
   echo Usage: %~dp0appveyor-script.bat "build"
 )
