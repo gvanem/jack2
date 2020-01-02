0dc8b5a4105ec034422e03dff9355646894c6aa4
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-02 11:48:41 +0000

    MSVC wants 'using namespace Jack;'.

--- common/JackMidiBufferWriteQueue.cpp
+++ common/JackMidiBufferWriteQueue.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include "JackMidiUtil.h"
 #include "JackError.h"
 
-using Jack::JackMidiBufferWriteQueue;
+using namespace Jack;
 
 JackMidiBufferWriteQueue::JackMidiBufferWriteQueue()
 {
