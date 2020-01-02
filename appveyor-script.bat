6e7882c2697184baee24c033757a904b3ea23047
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-02 13:06:05 +0000

    More tweaks.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -2,11 +2,15 @@
 setlocal
 if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0
 
-SET PATH=%PATH%;c:\MinGW\bin;"c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin"
+set PATH=%PATH%;c:\MinGW\bin
+set PORTAUDIO_ROOT=./windows/portaudio
+set PORTAUDIO_LIB=./windows/Release/bin/libportaudio_x86.a
+set LIBSAMPLERATE_ROOT=./windows
+set SAMPLERATE_LIB=./windows/Release/bin/libsamplerate_x86.a
 
 if %1. == build. (
   cd %APPVEYOR_BUILD_FOLDER%\src
-  mingw32-make -f Makefile.Windows USE_MP_COMPILE=0 USE_PTHREADS=0 CC=cl all
+  mingw32-make -f Makefile.Windows USE_MP_COMPILE=0 USE_PTHREADS=0 CC=gcc all
 ) else (
   echo Usage: %~dp0appveyor-script.bat "build"
 )
