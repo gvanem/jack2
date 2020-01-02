013f7545e5f953e26319ec46836678c106b677ef
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-02 12:35:08 +0000

    Try 'mingw32-make' instead.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -4,7 +4,7 @@ if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0
 
 if %1. == build. (
   cd %APPVEYOR_BUILD_FOLDER%\src
-  make -f Makefile.Windows CC=clang-cl all
+  mingw32-make -f Makefile.Windows CC=clang-cl all
 ) else (
   echo Usage: %~dp0appveyor-script.bat "build"
 )
