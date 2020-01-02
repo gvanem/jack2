a3749513c32d642b5cccf6449a273716c3092eff
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-02 12:56:13 +0000

    Set PATH first.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -2,9 +2,11 @@
 setlocal
 if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0
 
+SET PATH=%PATH%;c:\MinGW\bin;"c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin"
+
 if %1. == build. (
   cd %APPVEYOR_BUILD_FOLDER%\src
-  c:\MinGW\bin\mingw32-make -f Makefile.Windows USE_MP_COMPILE=0 CC=cl all
+  mingw32-make -f Makefile.Windows USE_MP_COMPILE=0 USE_PTHREADS=0 CC=cl all
 ) else (
   echo Usage: %~dp0appveyor-script.bat "build"
 )
