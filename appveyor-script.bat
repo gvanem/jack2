f3232dd1e41e5a7c775d699a921f68a8264645a0
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-02 18:25:16 +0000

    No need for 'APPVEYOR_BUILD_FOLDER'.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -1,15 +1,13 @@
 @echo off
 setlocal
-if "%APPVEYOR_BUILD_FOLDER%" == "" set APPVEYOR_BUILD_FOLDER=%~dp0
-
 set PATH=%PATH%;c:\MinGW\bin
 set PORTAUDIO_ROOT=./windows/portaudio
 set PORTAUDIO_LIB=./windows/Release/bin/libportaudio_x86.a
 set LIBSAMPLERATE_ROOT=./windows
 set SAMPLERATE_LIB=./windows/Release/bin/libsamplerate_x86.a
+set REGEX_ROOT=c:/MinGW/include
 
 if %1. == build. (
-  cd %APPVEYOR_BUILD_FOLDER%\src
   gcc -v
   mingw32-make -f Makefile.Windows USE_MP_COMPILE=0 USE_PTHREADS=0 CC=gcc all
 ) else (
