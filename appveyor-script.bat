9873b0350f58721ea1393eb05a3ea131de5119d0
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-03 13:01:05 +0000

    Use a local 'compat/regex/regex.c' file instead of an external '$(REGEX_LIB)'.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -1,18 +1,34 @@
 @echo off
 setlocal
-set PATH=%PATH%;c:\MinGW\bin
+::
+:: Set env-vars for 'MinGW' + 'mingw32-make'
+::
+set PATH=c:\msys64\MinGW32\bin;c:\msys64\usr\bin;%PATH%
+
+::
+:: Build with the bundled 'portaudio'
+::
 set PORTAUDIO_ROOT=./windows/portaudio
 set PORTAUDIO_LIB=./windows/Release/bin/libportaudio_x86.a
-set LIBSAMPLERATE_ROOT=./windows
+
+::
+:: Build with the bundled 'libsamplerate'
+::
+set SAMPLERATE_ROOT=./windows
 set SAMPLERATE_LIB=./windows/Release/bin/libsamplerate_x86.a
-set REGEX_ROOT=c:/MinGW/include
+
+::
+:: Do not build for Pthreads
+::
+set MAKE=make -f Makefile.Windows USE_PTHREADS=0
 
 if %1. == build. (
   gcc -v
-  mingw32-make -f Makefile.Windows USE_MP_COMPILE=0 USE_PTHREADS=0 CC=gcc all
-) else (
-  echo Usage: %~dp0appveyor-script.bat "build"
+  %MAKE% CC=gcc all
+  goto :EOF
 )
+
+echo Usage: %~dp0appveyor-script.bat "build"
 
 
 
