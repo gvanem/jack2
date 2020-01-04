1dc0f8b09bc92acb42d2b459c249392c49985fb0
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-04 12:54:50 +0000

    *) Simplify the 'appveyor-script.bat'. *) Add .res-files to .exe-files too. *) Preliminary 'make tests' target.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -3,23 +3,8 @@ setlocal
 ::
 :: Set env-vars for 'MinGW' + 'mingw32-make'
 ::
-set PATH=c:\msys64\MinGW32\bin;c:\msys64\usr\bin;%PATH%
-
-::
-:: Build with the bundled 'portaudio'
-::
-set PORTAUDIO_ROOT=./windows/portaudio
-set PORTAUDIO_LIB=./windows/Release/bin/libportaudio_x86.a
-
-::
-:: Build with the bundled 'libsamplerate'
-::
-set SAMPLERATE_ROOT=./windows
-set SAMPLERATE_LIB=./windows/Release/bin/libsamplerate_x86.a
-
-::
-:: Do not build for Pthreads
-::
+:: set PATH=c:\msys64\MinGW32\bin;c:\msys64\usr\bin;%PATH%
+set PATH=c:\msys64\MinGW32\bin;%PATH%
 set MAKE=make -f Makefile.Windows USE_PTHREADS=0
 
 ::
