3928d3ca3391e8d834af6efbe3037d6f398d93cb
Gisle Vanem <gisle.vanem@gmail.com>
2020-01-04 12:58:35 +0000

    It seems 'make' is in 'c:\msys64\usr\bin'.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -3,8 +3,7 @@ setlocal
 ::
 :: Set env-vars for 'MinGW' + 'mingw32-make'
 ::
-:: set PATH=c:\msys64\MinGW32\bin;c:\msys64\usr\bin;%PATH%
-set PATH=c:\msys64\MinGW32\bin;%PATH%
+set PATH=c:\msys64\MinGW32\bin;c:\msys64\usr\bin;%PATH%
 set MAKE=make -f Makefile.Windows USE_PTHREADS=0
 
 ::
